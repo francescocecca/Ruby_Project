33514fa991a297e952b5097c97f4c8e8628b504d
Francesco Pio Cecca <cecca.francesco@yahoo.it>
2024-01-12 16:30:57 +0100

    Json aggiustamenti

--- project/app/controllers/authors_controller.rb
+++ project/app/controllers/authors_controller.rb
@@ -1,17 +1,18 @@
 class AuthorsController < ApplicationController
   def get_all_authors
     @authors=Author.all
-    render json:{"data": @authors}
+    render json:{"data": @authors}, include: [:books]
   end
 
   def get_authors_by_id
     @author = Author.find(params[:id])
-    render json:{"data": @author}
+    render json:{"data": @author}, include: [:books]
   end
 
-  / Search with the space /
+  # Search with the space
+
   def get_by_name
     @author = Author.where(name: params[:name])
-    render json: {"data": @author}
+    render json: {"data": @author}, include: [:books]
   end
 end
